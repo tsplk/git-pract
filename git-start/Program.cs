0656205a058e6d17fbeb0b556b9dcef6f1bcccb4
TsyAy <82699322+TsyAy@users.noreply.github.com>
2022-05-17 17:35:15 +0800

    second commit

---
+++ git-start/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace git_start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Today 17.05.22 and started studying the Git system");
+            Console.WriteLine("I found out about:");
+            Console.WriteLine("git init, git add, git branch, pull, push (AND THEIR ADDITIONS).");
+        }
+    }
+}
