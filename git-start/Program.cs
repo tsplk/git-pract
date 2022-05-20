e8b5e58529c2c29e2e2f1009e921f396c2ecda21
TsyAy <82699322+TsyAy@users.noreply.github.com>
2022-05-20 13:25:47 +0800

    Добавление новой ветки и добавление новых конманд в список изученных

--- git-start/Program.cs
+++ git-start/Program.cs
@@ -6,9 +6,15 @@ namespace git_start
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Today 17.05.22 and started studying the Git system");
+            Console.WriteLine("Today 20.05.22 and studying the Git system");
             Console.WriteLine("I found out about:");
-            Console.WriteLine("git init, git add, git branch, pull, push (AND THEIR ADDITIONS).");
+            Console.WriteLine("git log, git show, git restore, git diff, git mv, git rm.");
+            Console.WriteLine("git log - показывает историю коммитов и их хэш");
+            Console.WriteLine("git show + hash - показывает подробности коммита");
+            Console.WriteLine("git restore + имя файла - откатывает состояние файла к последнему коммиту");
+            Console.WriteLine("git diff - показывает изменения в файлах вне индекса, а если добавить --staged внутри");
+            Console.WriteLine("git mv имя файла + *новое имя* - переименовывает имя файла, также если добавить вместо нового имени путь, то измениться путь");
+            Console.WriteLine("git rm имя файла - удалит файл везде, а сели добавть --cashed, то просто удалится из коммита, но останется в коллеции");
         }
     }
 }
